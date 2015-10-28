2cdabdadb2a1c5ed3423953186b591910462c8d0
Danesh M <daneshm90@gmail.com>
2015-10-28 14:07:03 -0700

    BatteryMeterDrawable: add text style (2/2)

--- src/com/shell/extras/fragments/StatusBarBattery.java
+++ src/com/shell/extras/fragments/StatusBarBattery.java
@@ -33,6 +33,8 @@ public class StatusBarBattery extends SettingsPreferenceFragment implements
 
 
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+    private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 2;
+
     private ListPreference mStatusBarBattery;
 
     @Override
@@ -48,6 +50,7 @@ public class StatusBarBattery extends SettingsPreferenceFragment implements
                 Settings.Secure.STATUS_BAR_BATTERY_STYLE, 0);
         mStatusBarBattery.setValue(String.valueOf(batteryStyle));
         mStatusBarBattery.setSummary(mStatusBarBattery.getEntry());
+        enableStatusBarBatteryDependents(batteryStyle);
         mStatusBarBattery.setOnPreferenceChangeListener(this);
 }
 
@@ -65,8 +68,17 @@ public class StatusBarBattery extends SettingsPreferenceFragment implements
             Settings.Secure.putInt(resolver,
                     Settings.Secure.STATUS_BAR_BATTERY_STYLE, batteryStyle);
             mStatusBarBattery.setSummary(mStatusBarBattery.getEntries()[index]);
+            enableStatusBarBatteryDependents(batteryStyle);
             return true;
         }
         return false;
+    }
+
+    private void enableStatusBarBatteryDependents(int batteryIconStyle) {
+        if (batteryIconStyle == STATUS_BAR_BATTERY_STYLE_TEXT) {
+            mStatusBarBatteryShowPercent.setEnabled(false);
+        } else {
+            mStatusBarBatteryShowPercent.setEnabled(true);
+        }
     }
 }
